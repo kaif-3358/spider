4312fdcc40af2344f52f681b8e8dd342e1c012a4
kaif-3358 <121166776+kaif-3358@users.noreply.github.com>
2023-11-01 19:15:51 +0530

    this ive just done for fun as they kinda funn

---
+++ spider.py
@@ -0,0 +1,12 @@
+name = input ("What is your name")
+print ("Hello",name)
+
+web = int(input ("How many spiders are there in there in the attic"))
+
+if web >20:
+    print ("so many creepy crawlies")
+else:
+    print ("Listen you pull down the webs and i get the broom")
+print ("OK bye",name)
+
+print("..")
